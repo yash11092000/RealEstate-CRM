754ca0ba350aec918bcb420065529f4db1023086
Admin <yash.bhosale1109@gmail.com>
2025-11-07 00:28:26 +0530

    #Added UI Changes Sidebar related

---
+++ PhysioWeb/Models/SidebarMenu.cs
@@ -0,0 +1,20 @@
+﻿namespace PhysioWeb.Models
+{
+    public class SidebarMenu:CommanProp
+    {
+        public string MenuName { get; set; }
+        public string MenuUrl { get; set; }
+        public string MenuType { get; set; }  // parent or child
+        public int? ParentMenuId { get; set; }
+        public string MenuIcon { get; set; }
+        public int DisplayOrder { get; set; }
+        public bool IsActive { get; set; }
+        public bool OpenInNewTab { get; set; }
+
+        public List<DropDownSource> ParentList { get; set; }
+        public SidebarMenu()
+        {
+            ParentList = new List<DropDownSource>();
+        }
+    }
+}
