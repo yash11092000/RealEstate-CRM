aa001f14f8a8f67fbbc27ba8aa63daa01bd8f089
Admin <yash.bhosale1109@gmail.com>
2026-02-02 22:43:45 +0530

    #Lead Dashboard Ui changes And Data Binding

--- PhysioWeb/Controllers/DashboardController.cs
+++ PhysioWeb/Controllers/DashboardController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using PhysioWeb.Models;
+using PhysioWeb.Repository;
 
 namespace PhysioWeb.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly IDashboardRepository _IDashboardRepository;
+        public DashboardController(IDashboardRepository dashboardRepository)
+        {
+            _IDashboardRepository = dashboardRepository;
+        }
         public async Task<IActionResult> LeadDashboard()
         {
-            return View();
+            string UserID = User.FindFirst(ClaimTypes.PrimarySid)?.Value;
+            string AgencyId = User.FindFirst(ClaimTypes.GroupSid)?.Value;
+            LeadDashboard data = await _IDashboardRepository.GetLeadDashboardData(UserID, AgencyId);
+            return View(data);
         }
 
         public async Task<ActionResult> SalesDashboard()
---
+++ PhysioWeb/Repository/DashboardRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using PhysioWeb.Data;
+using PhysioWeb.Models;
+
+namespace PhysioWeb.Repository
+{
+    public class DashboardRepository : IDashboardRepository
+    {
+        private readonly DbHelper _dbHelper;
+
+        public DashboardRepository(DbHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+
+        }
+        public async Task<LeadDashboard> GetLeadDashboardData(string? userID, string? agencyId)
+        {
+            try
+            {
+                string[] parametersName = { "UserID", "AgencyId" };
+                object[] Values = { userID, agencyId };
+
+                string Sp = "FMR_GetLeadDashboardData";
+                var data = await _dbHelper.GetDataReaderAsync(Sp, parametersName, Values);
+
+                while (data.Read())
+                {
+                    var LeadData = new LeadDashboard(data);
+                    return LeadData;
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                // Optional: log error here
+                throw;
+            }
+        }
+    }
+}
