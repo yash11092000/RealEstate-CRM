8da4d496861d7a9f7406ed96da07e7fe5a9720b9
nikitakasab <nikikasab1419@gmail.com>
2026-02-08 15:29:11 +0530

    #GeneralSettings

---
+++ PhysioWeb/Models/GeneralSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace PhysioWeb.Models
+{
+    public class GeneralSettings : CommanProp
+    {
+        public string Prefix { get; set; }
+        public string SuffixCodeNo { get; set; }
+    }
+}
